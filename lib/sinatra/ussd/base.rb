c743928f6020480c52f66436f63c4a3ac4512ffa
Anuj Jamwal <anuj.jamwal1@gmail.com>
2014-05-06 12:40:35 +0530

    #5 Now it allows passing cache_store using set in sinatra

--- lib/sinatra/ussd/base.rb
+++ lib/sinatra/ussd/base.rb
@@ -3,8 +3,11 @@ module Sinatra
     module Base
 
       def self.registered(app)
+        cache_store = app.settings.respond_to?(:cache_store) ? app.settings.cache_store : Sinatra::Ussd::CacheStore.instance
+        caching = Sinatra::Ussd::Caching.new(cache_store)
+
         app.use Sinatra::Ussd::Middleware::Facade
-        app.use Sinatra::Ussd::Middleware::Echo
+        app.use Sinatra::Ussd::Middleware::Echo, caching
         app.use Sinatra::Ussd::Middleware::MessageBuilder
         app.use Sinatra::Ussd::Middleware::Dispatcher
 
